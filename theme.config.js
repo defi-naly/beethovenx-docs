212359d7b333ccd9e689eb7de4a8498db3fdee39
groninge <groninge@gmail.com>
2023-02-09 14:48:48 +0100

    add shortcut icon too

--- theme.config.js
+++ theme.config.js
@@ -125,6 +125,7 @@ export default {
                 <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
                 <link rel="manifest" href="/favicon/site.webmanifest" />
                 <link rel="mask-icon" href="/favicon/safari-pinned-tab.svg" color="#000000" />
+                <link rel="shortcut icon" href="/favicon/favicon.ico" />
                 <meta name="msapplication-TileColor" content="#ffffff" />
                 <meta httpEquiv="Content-Language" content="en" />
                 <meta
